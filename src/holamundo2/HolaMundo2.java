11fcb103286ce35786a17c75605ddde97aae1425
Carmen Santana Enríquez <csantanaenriquez@gmail.com>
2019-04-01 12:15:33 +0200

    Modificada la clase principalotra vez

--- src/holamundo2/HolaMundo2.java
+++ src/holamundo2/HolaMundo2.java
@@ -6,6 +6,7 @@ public class HolaMundo2 {
     public static void main(String[] args) {
         System.out.println("Hola Mundo2");
         System.out.println("Abril 2019");
+        System.out.println("IES Luis Vélez de Guevara");
     }
     
 }
