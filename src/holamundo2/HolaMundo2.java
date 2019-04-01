ab3de18f38d4a466bf958479c300798c76a170cd
Carmen Santana Enríquez <csantanaenriquez@gmail.com>
2019-04-01 12:37:36 +0200

    otra vez más modificamos

--- src/holamundo2/HolaMundo2.java
+++ src/holamundo2/HolaMundo2.java
@@ -7,7 +7,7 @@ public class HolaMundo2 {
         System.out.println("Hola Mundo2");
         System.out.println("Abril 2019");
         System.out.println("IES Luis Vélez de Guevara");
-        System.out.println("Otra vez");
+        System.out.println("Andalucía");
     }
     
 }
