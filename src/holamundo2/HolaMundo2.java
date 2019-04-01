83890e6b45a78cf957f1de0ec1ca60f31bc7ee3e
Carmen Santana Enríquez <csantanaenriquez@gmail.com>
2019-04-01 12:13:29 +0200

    Modificada la clase principal

---
+++ src/holamundo2/HolaMundo2.java
@@ -0,0 +1,11 @@
+
+package holamundo2;
+
+public class HolaMundo2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo2");
+        System.out.println("Abril 2019");
+    }
+    
+}
